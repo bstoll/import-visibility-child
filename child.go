b92c075e246327febd4bbf4f2249364efd523254
Brandon Stoll <bstoll@users.noreply.github.com>
2025-01-30 20:05:18 +0000

    Commit v0.2.0

--- child.go
+++ child.go
@@ -2,8 +2,11 @@ package child
 
 import (
 	"fmt"
+
+	parent "github.com/bstoll/import-visibility-parent"
 )
 
 func Child() {
-	fmt.Println("Hello from child v1")
+	fmt.Println("Hello from child v2")
+	parent.Parent()
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/bstoll/import-visibility-child
 
 go 1.23.4
+
+require github.com/bstoll/import-visibility-parent v0.1.0
